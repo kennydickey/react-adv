1095fcf2cc7a038184a94bc33d0cfb4ee9d55c9a
seatacken <57050116+seatacken@users.noreply.github.com>
2020-07-06 23:54:22 -0700

    end of single component

--- oldFashionedWay/index.js
+++ oldFashionedWay/index.js
@@ -4,9 +4,13 @@ function getMood() {
 	//return a random mood based on random index of moods arr
 	return moods[Math.floor(Math.random() * moods.length)];
 }
+function getNum() {
+	return Math.floor(Math.random() * 10) + 1;
+}
 
 class Hello extends React.Component {
 	render() {
+		const num = getNum();
 		return (
 			//jsx here vv
 			<div>
@@ -14,7 +18,14 @@ class Hello extends React.Component {
 				<h1>Hello there!</h1>
 				<h1>embedded js 2x4 {2 * 4}</h1>
 				<h1>current mood is {getMood()}</h1>
-				<img src="https://images.unsplash.com/photo-1551726195-9d356d629367?ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80"/>
+				<h1>random num is {num}</h1>
+				{/* ternary if num = 7 this else that*/}
+				<p>{num === 7 ? 'congrats!' : 'wrong num :('}</p>
+				{/* multi line ternary */}
+				{
+					num === 7 ? <img src="https://images.unsplash.com/photo-1551726195-9d356d629367?ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80"/>
+					: null
+				}
 			</div>
 		);
 	}
