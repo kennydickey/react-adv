10071da4fcc813f6af55b9d58f86f5ceb4fa1180
seatacken <57050116+seatacken@users.noreply.github.com>
2020-07-07 21:37:22 -0700

    using props

--- oldFashionedWay/NumPicker.js
+++ oldFashionedWay/NumPicker.js
@@ -7,7 +7,9 @@ class NumPicker extends React.Component {
 				<h1>random num is {num}</h1>
 				{/* ternary if num = 7 this else that*/}
 				<p>{num === 7 ? 'congrats!' : 'wrong num :('}</p>
-				{/* multi line ternary */}
+    {/* img using props */}
+    <img src={this.props.img} />
+    {/* multi line ternary */}
 				{
 					num === 7 ? <img src="https://images.unsplash.com/photo-1551726195-9d356d629367?ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80"/>
 					: null
--- oldFashionedWay/index.js
+++ oldFashionedWay/index.js
@@ -3,8 +3,16 @@ class App extends React.Component {
 		return (
 		//div here allows us to place multiple components
 		<div>
-				<Hello />
-				<NumPicker />
+			{/* with our props (properties) */}
+				<Hello 
+				to='Ringo' 
+				from='Paul'
+				num={3} //must be str or barackets, will not parse num=3 for ex
+				data={[1,2,3,4]}
+				isFunny // same as saying isFunny={true}
+				bangs={4}
+				/>
+				<NumPicker img="https://i.pinimg.com/originals/17/5e/2f/175e2f55ecb45be4927d457e9dbfaab3.jpg"/>
 		</div>
 		)
 		}
