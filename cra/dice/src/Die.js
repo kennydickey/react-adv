64316c28c177ac6b741afde9f5d74d2b681511c7
seatacken <57050116+seatacken@users.noreply.github.com>
2020-07-25 19:12:17 -0700

    icon rendering

---
+++ cra/dice/src/Die.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Die extends Component {
+ render(){
+  // return <h1>die</h1>
+  return <i className="fas fa-dice"></i>
+ }
+}
+export default Die;
